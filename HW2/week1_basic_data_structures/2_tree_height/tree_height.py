391db32fbf52bcbe1bf98185261bf1d1e179641f
Ashkan Yousefi <ashkan.yousefi@gmail.com>
2019-12-21 12:53:53 +0330

    updated to the final version

--- HW2/week1_basic_data_structures/2_tree_height/tree_height.py
+++ HW2/week1_basic_data_structures/2_tree_height/tree_height.py
@@ -1,32 +1,32 @@
-# # python3
-
-# import sys
-# import threading
-
-
-# def compute_height(n, parents):
-#     # Replace this code with a faster implementation
-#     max_height = 0
-#     for vertex in range(n):
-#         height = 0
-#         current = vertex
-#         while current != -1:
-#             height += 1
-#             current = parents[current]
-#         max_height = max(max_height, height)
-#     return max_height
-
-# def main():
-#     n = int(input())
-#     parents = list(map(int, input().split()))
-#     print(compute_height(n, parents))
-
-# # In Python, the default limit on recursion depth is rather low,
-# # so raise it here for this problem. Note that to take advantage
-# # of bigger stack, we have to launch the computation in a new thread.
-# sys.setrecursionlimit(10**7)  # max depth of recursion
-# threading.stack_size(2**27)   # new thread will get stack of such size
-# threading.Thread(target=main).start()
+# python3
+ 
+import sys, threading
+sys.setrecursionlimit(10**7) # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
+ 
+class TreeHeight:
+        def read(self):
+                self.n = int(sys.stdin.readline())
+                self.parent = list(map(int, sys.stdin.readline().split()))
+ 
+        def compute_height(self):
+                # Replace this code with a faster implementation
+                maxHeight = 0
+                for vertex in range(self.n):
+                        height = 0
+                        i = vertex
+                        while i != -1:
+                                height += 1
+                                i = self.parent[i]
+                        maxHeight = max(maxHeight, height)
+                return maxHeight
+ 
+def main():
+  tree = TreeHeight()
+  tree.read()
+  print(tree.compute_height())
+ 
+threading.Thread(target=main).start()
 
 number_size=5
 child_arrangement=['-1','0','4','0','3']
